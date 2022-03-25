089f0d270247c87ab5eb44574257b03cbe13ec4a
Lucas Tabare <lucas.tabare@gmail.com>
2022-03-25 20:29:18 -0300

    Estructurando el proyecto

---
+++ src/firebaseConfig/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9D6Q2Vzg_b7TDZ2lLcI9vwW5lvLkus1Q",
+  authDomain: "productos-906bf.firebaseapp.com",
+  projectId: "productos-906bf",
+  storageBucket: "productos-906bf.appspot.com",
+  messagingSenderId: "27124440286",
+  appId: "1:27124440286:web:cfc888ce10bbb5cdcd364f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
