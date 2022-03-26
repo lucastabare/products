1e477ea43fb79982e6748514702e6e5bd5620bf8
Lucas Tabare <lucas.tabare@gmail.com>
2022-03-25 21:50:05 -0300

    Creando la funcionalidad Show

---
+++ src/components/Show.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { collection, getDocs, getDoc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebaseConfig/firebase";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { async } from "@firebase/util";
+
+const MySwal = withReactContent(Swal);
+
+const Show = () => {
+  //1-configurar hooks
+  const [products, setProducts] = useState([]);
+  //2-referencia a la DB firestore
+  const productsCollection = collection(db, "products");
+  //3-funcion para mostrar TODOS los docs
+  const getProducts = async () => {
+    const data = await getDocs(productsCollection);
+    //console.log(data.docs);
+    setProducts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    console.log(products);
+  };
+  //4-funcion para eliminar un doc
+  const deleteProduct = async (id) => {
+    const productDoc = doc(dc, "products", id);
+    await deleteDoc(productDoc);
+    getProducts();
+  };
+  //5-funcion de confirmacion para Sweet Alert
+
+  //6-usamos useEffect
+  useEffect(() => {
+    getProducts();
+  }, []);
+  //7-devolvemos vista de nuestro componente
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="d-grid gap-2">
+              <Link to="/create" className="btn btn-secondary mt-2 mb-2">
+                Create
+              </Link>
+            </div>
+            <table className="table table-dark table-hover">
+              <thead>
+                <th>Descripcion</th>
+                <th>Stock</th>
+                <th>Acciones</th>
+              </thead>
+
+              <tbody>
+                {products.map((product) => (
+                  <tr key={product.id}>
+                    <td>{product.description}</td>
+                    <td>{product.stock}</td>
+                    <td>
+                      <Link
+                        to={`/edit/${product.id}`}
+                        className="btn btn-light"
+                      >
+                        {" "}
+                        <i className="fa-solid fa-pencil"></i>
+                      </Link>
+                      <button
+                        onClick={() => {
+                          deleteProduct(product.id);
+                        }}
+                        className="btn btn-danger"
+                      >
+                        <i className="fa-solid fa-trash-can"></i>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Show;
--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "@firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC9D6Q2Vzg_b7TDZ2lLcI9vwW5lvLkus1Q",
   authDomain: "productos-906bf.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:27124440286:web:cfc888ce10bbb5cdcd364f",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
